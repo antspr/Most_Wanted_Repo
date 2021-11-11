cee5cd6719151df5b86b0adb020db597c4c1518c
antspr <antoinesprings@outlook.com>
2021-11-11 14:00:10 -0500

    commit

--- Most_Wanted_Repo/app.js
+++ Most_Wanted_Repo/app.js
@@ -70,6 +70,10 @@ function app(people){
 
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
+if (searchResults.length > 1){
+  person = searchBy(searchResults);
+}
+
    
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
